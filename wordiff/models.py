e68c7eaa7a7a08ddf3da9f10479db026e3605ef0
Llewellyn Hinkes-Jones <lhinkesjones@theatlantic.com>
2013-02-27 12:09:40 -0500

    Adding recent matching algorithm

--- wordiff/models.py
+++ wordiff/models.py
@@ -2,10 +2,56 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
+class ObjectGramManager(models.Manager):
+
+	def most_recent_matches(content_type_id, offset=0, count=20):
+	
+		""" 
+		Grabs the most recent groups of texts with common n-gram sets
+		and returns a list of dicts containing the n-grams, and the sets of texts
+		"""
+
+		ngrams = GramRankings.objects.all().filter(content_type=content_type_id).order_by("-ranking", "-date_recent_match")[offset:count]
+		
+		model_class = ContentType.objects.get(id=content_type_id).model_class()
+		
+		trending_similarities = []
+		
+		for ngram in ngrams:
+			common_texts = model_class.objects.filter(id__in=ObjectGram.objects.filter(gram=ngram.gram).values_list("object_id"))
+			
+			trending_similarities.append({ "ngram":ngram, "common_texts": common_texts })
+		
+		return trending_similarities
 
 class ObjectGram(models.Model):
-	content_type = models.ForeignKey(ContentType)
-	object_id = models.PositiveIntegerField()
+
+	objects = ObjectGramManager()
+	
+	content_type = models.ForeignKey(ContentType, null=False, blank=False)
+	
+	object_id = models.PositiveIntegerField(null=False, blank=False)
+	
 	content_object = generic.GenericForeignKey("content_type", "object_id")
 	
-	gram = models.CharField(max_length=1000)
+	gram = models.CharField(max_length=1000, null=False, blank=False)
+
+	date_created = models.DateTimeField(auto_now_add=True)
+	
+	def __unicode__(self, *args, **kwargs):
+		return self.gram
+	
+
+class GramRankings(models.Model):
+
+	gram = models.CharField(max_length=1000, null=False, blank=False)
+	
+	ranking = models.PositiveIntegerField(null=False, blank=False)
+	
+	content_type = models.ForeignKey(ContentType, null=False, blank=False)
+	
+	date_recent_match = models.DateTimeField()
+	
+	date_created = models.DateTimeField(auto_now_add=True)
+	
+	date_modified = models.DateTimeField(auto_now=True)
--- wordiff/tasks.py
+++ wordiff/tasks.py
@@ -1,8 +1,24 @@
-from wordiff.models import ObjectGram
+from wordiff.models import ObjectGram, GramRankings
 from wordiff.n_gram_splitter import lang_model
 from django.conf import settings
+from HTMLParser import HTMLParser
 
 NGRAM_LENGTH = 8
+
+class MLStripper(HTMLParser):
+	def __init__(self):
+		self.reset()
+		self.fed = []
+	def handle_data(self, d):
+		self.fed.append(d)
+	def get_data(self):
+		return ''.join(self.fed)
+
+def strip_tags(html):
+	s = MLStripper()
+	s.feed(html)
+	return s.get_data()
+
 
 if hasattr(settings, "WORDIFF_NGRAM_LENGTH"):
 	NGRAM_LENGTH = settings.WORDIFF_NGRAM_LENGTH
@@ -10,11 +26,12 @@ if hasattr(settings, "WORDIFF_NGRAM_LENGTH"):
 def gram_parse_object_text(object, object_text):
 	""" 
 	Parses out an object into its N-grams and saves them
-	Can be included as a task, celery task, a post-save signal, or part of the save method
+	Can be included as a task, celery task, a post-save signal, 
+	or part of the save method
 	but may need to be modified accordingly
 	"""
 	
-	parsed = lang_model(object_text)
+	parsed = lang_model(strip_tags(object_text))
 	
 	for gram in parsed.gram(NGRAM_LENGTH):
 		object_gram = ObjectGram()
@@ -23,6 +40,34 @@ def gram_parse_object_text(object, object_text):
 		object_gram.save()
 		
 
+
+def update_gram_rankings():
+	object_grams = ObjectGram.objects.all()
+	#ObjectGram.objects.distinct("content_type").all()
+	
+	GramRankings.objects.all().delete()
+	
+	for object_gram in object_grams:
+		rankings = ObjectGram.objects.raw("\
+		SELECT id, gram, COUNT(*) as quantity, MAX(date_created) as date_recent_match FROM wordiff_objectgram WHERE \
+		content_type_id='%s'\
+		GROUP BY gram ORDER BY quantity DESC LIMIT 20\
+		" % object_gram.content_type_id)
+		
+		
+	inserts = []
+	
+	for rank in rankings:
+		if rank.quantity > 1:
+			ranking = GramRankings()
+			ranking.gram = rank.gram
+			ranking.ranking = rank.quantity
+			ranking.content_type = rank.content_type
+			ranking.date_recent_match = rank.date_recent_match
+			inserts.append(ranking)
+	
+	if inserts:
+		GramRankings.objects.bulk_create(inserts)
 
 """ 
 
@@ -37,8 +82,8 @@ from blog.models import Post
 
 @receiver(post_save, sender=Post)
 def post_save_gram_parse(sender, instance, created, **kwargs):
-    if created:
-    	gram_parse_object_text(instance, instance.content)
+	if created:
+		gram_parse_object_text(instance, instance.content)
 
 
 """
